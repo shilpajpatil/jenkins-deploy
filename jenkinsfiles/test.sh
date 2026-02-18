f1f65ab18f492c5e88082aa45490887f02a9ed05
shilpajpatil <40339744+shilpajpatil@users.noreply.github.com>
2026-02-18 12:36:27 +0530

    Create test.sh

---
+++ jenkinsfiles/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Hello, this is a basic test script"
+echo "Current Date:"
+date
+echo "Current User:"
+whoami
+echo "Current Directory:"
+pwd
+
+echo "Script executed successfully!"
